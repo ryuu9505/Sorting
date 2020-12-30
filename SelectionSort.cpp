596b87897d21b7f9d44a5be4c45f89065f793d27
ryuu9505 <66634707+ryuu9505@users.noreply.github.com>
2020-12-31 05:43:33 +0900

    version 1.1

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 
-void SelctionSort(int arr[])
+void SelectionSort(array<int, 10> arr)
 {
-    for (int i=1; i<arr.size(); i++)
+    int temp, idx_min, i, j;
+
+    for (i = 1; i < arr.size(); i++)
     {
-        for (j=i; j>arr.size(); j++)
-            if (arr[i-1] > arr[j])
+        for (j = i; j > arr.size(); j++)
+            if (arr[i - 1] > arr[j])
                 idx_min = j;
-        temp = arr[i-1];
-        arr[i-1] = arr[idx_min];
-        arr[idx_min] = temp; 
-    }   
+        temp = arr[i - 1];
+        arr[i - 1] = arr[idx_min];
+        arr[idx_min] = temp;
+    }
 }
 
 int main()
@@ -24,9 +26,9 @@ int main()
 
     uniform_int_distribution<int> dis(0, 99);
 
-    int arr[10];
-    for (int i = 0; i<arr.size(); i++)
-        arr[i] = dis(gen); 
+    array<int, 10> arr;
+    for (int i = 0; i < arr.size(); i++)
+        arr[i] = dis(gen);
 
     for (int i = 0; i < arr.size(); i++)
         cout << arr[i] << " ";
